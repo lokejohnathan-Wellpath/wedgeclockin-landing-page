c2166f46fece065bf4a227aebb282263f53f7c26
lokejohnathan-Wellpath <loke.johnathan@gmail.com>
2026-09-12 11:48:04 +0800

    Add management P&L inside Wedge-I

---
+++ app/wedge-i/pnl/page.tsx
@@ -0,0 +1,11 @@
+import ManagementPnlReport from "../../components/ManagementPnlReport";
+
+export default function WedgeIPnlPage() {
+  return (
+    <ManagementPnlReport
+      backHref="/wedge-i"
+      backLabel="Wedge-I"
+      eyebrow="WEDGE-I · MANAGEMENT P&L"
+    />
+  );
+}
